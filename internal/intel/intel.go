0bbaee8d262a9ec3790bed7dc1daa877f48bc24a
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-01-31 16:18:47 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -8,59 +8,50 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/nyaruka/phonenumbers"
+	"github.com/nyaruka/phonenumbers" // Ensure this matches go.mod
 )
 
 type FullIntel struct {
-	IP          string  `json:"query"`
-	Status      string  `json:"status"`
-	Country     string  `json:"country"`
-	RegionName  string  `json:"regionName"`
-	City        string  `json:"city"`
-	Zip         string  `json:"zip"`
-	Lat         float64 `json:"lat"`
-	Lon         float64 `json:"lon"`
-	ISP         string  `json:"isp"`
-	Org         string  `json:"org"`
-	AS          string  `json:"as"`
-	ReverseDNS  string  `json:"reverse"`
+	IP         string  `json:"query"`
+	Status     string  `json:"status"`
+	Country    string  `json:"country"`
+	RegionName string  `json:"regionName"`
+	City       string  `json:"city"`
+	Zip        string  `json:"zip"`
+	Lat        float64 `json:"lat"`
+	Lon        float64 `json:"lon"`
+	ISP        string  `json:"isp"`
+	Org        string  `json:"org"`
+	AS         string  `json:"as"`
+	ReverseDNS string  `json:"reverse"`
 }
 
-// GetIntel pulls everything: Geo, ISP, Org, Zip, and Hostname
-func GetIntel(target string) (*FullIntel, error) {
-	// Querying ip-api with all necessary fields
-	url := fmt.Sprintf("http://ip-api.com/json/%s?fields=status,message,country,regionName,city,zip,lat,lon,isp,org,as,reverse,query", target)
+func GetIntel(target string) (*FullIntel, string, error) {
+	// IP-API for Geo + ISP + Zip + Org
+	url := fmt.Sprintf("http://ip-api.com/json/%s?fields=66846719", target)
 	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, "", err }
 	defer resp.Body.Close()
 
 	var data FullIntel
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		return nil, err
-	}
-	return &data, nil
-}
+	json.NewDecoder(resp.Body).Decode(&data)
 
-// Whois fetches the registrar and handler info
-func Whois(target string) string {
+	// WHOIS for Handler details
+	whois := "Whois data unavailable"
 	conn, err := net.DialTimeout("tcp", "whois.iana.org:43", 5*time.Second)
-	if err != nil {
-		return "Whois unavailable"
+	if err == nil {
+		defer conn.Close()
+		fmt.Fprintf(conn, "%s\r\n", target)
+		res, _ := io.ReadAll(conn)
+		whois = string(res)
 	}
-	defer conn.Close()
-	conn.Write([]byte(target + "\r\n"))
-	res, _ := io.ReadAll(conn)
-	return string(res)
+
+	return &data, whois, nil
 }
 
-// PhoneLookup provides carrier and region info
 func PhoneLookup(number string) string {
 	num, err := phonenumbers.Parse(number, "")
-	if err != nil {
-		return "Invalid number format"
-	}
+	if err != nil { return "Invalid Format" }
 	region := phonenumbers.GetRegionCodeForNumber(num)
 	return fmt.Sprintf("Region: %s | Valid: %v | Type: %s", region, phonenumbers.IsValidNumber(num), phonenumbers.GetNumberType(num))
 }
