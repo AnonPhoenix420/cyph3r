5a36242a9ee7033603e93866b8d267f47844a936
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-01 17:59:56 -0500

    Update colors.go

--- internal/output/colors.go
+++ internal/output/colors.go
@@ -1,11 +1,18 @@
 package output
+
 import "github.com/fatih/color"
+
 var (
-	CyanText = color.New(color.FgCyan).SprintFunc()
-	WhiteText = color.New(color.FgWhite, color.Bold).SprintFunc()
+	CyanText   = color.New(color.FgCyan).SprintFunc()
+	WhiteText  = color.New(color.FgWhite, color.Bold).SprintFunc()
 	YellowText = color.New(color.FgYellow).SprintFunc()
-	BlueText = color.New(color.FgBlue).SprintFunc()
-	RedText = color.New(color.FgRed).SprintFunc()
-	GreenText = color.New(color.FgGreen).SprintFunc()
-	MagentaText = color.New(color.FgMagenta).SprintFunc()
+	BlueText   = color.New(color.FgBlue).SprintFunc()
+	RedText    = color.New(color.FgRed).SprintFunc()
+	GreenText  = color.New(color.FgGreen).SprintFunc()
+	MagText    = color.New(color.FgMagenta).SprintFunc()
 )
+
+// Helper for generic info lines
+func Info(m string) {
+	fmt.Printf("%s %s\n", BlueText("[*]"), m)
+}
