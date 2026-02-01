fb209d120671134e110585b16399287dd5b9060b
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-01 18:29:19 -0500

    Update banner.go

--- internal/output/banner.go
+++ internal/output/banner.go
@@ -1,3 +1,4 @@
+cat <<EOF > internal/output/banner.go
 package output
 
 import (
@@ -6,13 +7,13 @@ import (
 )
 
 func Banner() {
-	banner := `
+	banner := \`
    ______      ____  __  __ _____ ____ 
-  / ____/_  __/ __ \/ / / /|__  // __ \
+  / ____/_  __/ __ \/ / / /|__  // __ \\
  / /   / / / / /_/ / /_/ /  /_ </ /_/ /
 / /___/ /_/ / ____/ __  / ___/ / _, _/ 
 \____/\__, /_/   /_/ /_/ /____/_/ |_|  
-     /____/         NETWORK_INTEL_SYSTEM`
+     /____/         NETWORK_INTEL_SYSTEM\`
 
 	fmt.Println(CyanText(banner))
 	fmt.Printf("\n %s\n", WhiteText("⚡ v2.6 [STABLE] // Wireframe HUD Edition"))
@@ -24,3 +25,4 @@ func ScanAnimation() {
 	time.Sleep(600 * time.Millisecond)
 	fmt.Println(GreenText("[READY]"))
 }
+EOF
