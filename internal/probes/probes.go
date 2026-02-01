603330b8eebe6dd4f47b3dd8fcf7c05b04bdf2cd
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-01 14:32:20 -0500

    Update probes.go

--- internal/probes/probes.go
+++ internal/probes/probes.go
@@ -1,43 +1,18 @@
 package probes
-
 import (
-	"fmt"
 	"net"
 	"net/http"
 	"time"
 )
-
 func ExecuteProbe(proto, target string, port int) (bool, time.Duration) {
 	start := time.Now()
-	addr := net.JoinHostPort(target, fmt.Sprintf("%d", port))
-
-	switch proto {
-	case "tcp", "ack":
-		conn, err := net.DialTimeout("tcp", addr, 3*time.Second)
-		if err == nil {
-			conn.Close()
-			return true, time.Since(start)
-		}
-	case "udp":
-		conn, err := net.DialTimeout("udp", addr, 2*time.Second)
-		if err == nil {
-			conn.Close()
-			return true, time.Since(start)
-		}
-	case "http", "https":
-		client := http.Client{Timeout: 5 * time.Second}
-		url := fmt.Sprintf("%s://%s:%d", proto, target, port)
-		resp, err := client.Get(url)
-		if err == nil {
-			resp.Body.Close()
-			return true, time.Since(start)
-		}
-	case "ping":
-		conn, err := net.DialTimeout("tcp", addr, 2*time.Second)
-		if err == nil {
-			conn.Close()
-			return true, time.Since(start)
-		}
+	addr := fmt.Sprintf("%s:%d", target, port)
+	if proto == "http" || proto == "https" {
+		c := &http.Client{Timeout: 2 * time.Second}
+		_, err := c.Get(proto + "://" + target)
+		return err == nil, time.Since(start)
 	}
-	return false, 0
+	conn, err := net.DialTimeout("tcp", addr, 2*time.Second)
+	if err == nil { conn.Close() }
+	return err == nil, time.Since(start)
 }
