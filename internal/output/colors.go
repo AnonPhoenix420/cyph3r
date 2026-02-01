8e6ddfe601f80f91d93e7dcf2e184ad4ee891069
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-01 14:29:27 -0500

    Update colors.go

--- internal/output/colors.go
+++ internal/output/colors.go
@@ -1,13 +1,11 @@
 package output
-
 import "github.com/fatih/color"
-
 var (
-	CyanText    = color.New(color.FgCyan).SprintFunc()
-	WhiteText   = color.New(color.FgWhite, color.Bold).SprintFunc()
-	YellowText  = color.New(color.FgYellow).SprintFunc()
+	CyanText = color.New(color.FgCyan).SprintFunc()
+	WhiteText = color.New(color.FgWhite, color.Bold).SprintFunc()
+	YellowText = color.New(color.FgYellow).SprintFunc()
+	BlueText = color.New(color.FgBlue).SprintFunc()
+	RedText = color.New(color.FgRed).SprintFunc()
+	GreenText = color.New(color.FgGreen).SprintFunc()
 	MagentaText = color.New(color.FgMagenta).SprintFunc()
-	BlueText    = color.New(color.FgBlue).SprintFunc()
-	RedText     = color.New(color.FgRed).SprintFunc()
-	GreenText   = color.New(color.FgGreen).SprintFunc()
 )
