7eb1d3ea450ae355b2d03c19b936dc934f406563
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-08 15:02:05 -0500

    Update status.go

--- internal/output/status.go
+++ internal/output/status.go
@@ -5,3 +5,7 @@ import "fmt"
 func Error(msg string) {
 	fmt.Printf("%s[!] ERROR: %s%s%s\n", NeonPink, White, msg, Reset)
 }
+
+func PulseNode(target string) {
+	fmt.Printf("%s[*] ESTABLISHING PULSE WITH: %s%s%s\n", White, NeonPink, target, Reset)
+}
