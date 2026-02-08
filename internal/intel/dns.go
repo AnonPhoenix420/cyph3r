098f93372f6fd367813dacc40d3d537c15c80921
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-08 14:42:28 -0500

    Update dns.go

--- internal/intel/dns.go
+++ internal/intel/dns.go
@@ -1,27 +1,12 @@
 package intel
 
-import (
-	"net"
-)
+import "net"
 
-// LookupNodes resolves the A and NS records for a target.
 func LookupNodes(target string) ([]string, []string) {
-	var ips []string
-	var nss []string
-
-	// Resolve IPs
-	addr, err := net.LookupHost(target)
-	if err == nil {
-		ips = addr
-	}
-
-	// Resolve Nameservers
-	nsRecords, err := net.LookupNS(target)
-	if err == nil {
-		for _, ns := range nsRecords {
-			nss = append(nss, ns.Host)
-		}
-	}
-
+	var ips, nss []string
+	addr, _ := net.LookupHost(target)
+	ips = addr
+	nsRecords, _ := net.LookupNS(target)
+	for _, ns := range nsRecords { nss = append(nss, ns.Host) }
 	return ips, nss
 }
