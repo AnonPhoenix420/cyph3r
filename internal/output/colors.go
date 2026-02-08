d0a8733a1077c29c94e2f001cccefcbd3f9c6975
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-08 14:44:19 -0500

    Update colors.go

--- internal/output/colors.go
+++ internal/output/colors.go
@@ -1,20 +1,9 @@
 package output
 
-// Neon Tech Color Palette
-// Using 256-color ANSI escape sequences for high-vibrancy output
 const (
-	Reset      = "\033[0m"
-	White      = "\033[37m"
-	
-	// Neon Blue (Banner & IP Addresses)
-	NeonBlue   = "\033[38;5;45m"
-	
-	// Neon Pink (Web Links / Fatal Errors)
-	NeonPink   = "\033[38;5;201m"
-	
-	// Neon Green (Port Status / Success)
-	NeonGreen  = "\033[38;5;82m"
-	
-	// Neon Yellow (GeoIP / NS Nodes)
-	NeonYellow = "\033[38;5;226m"
+	Reset     = "\033[0m"
+	NeonGreen = "\033[38;5;121m"
+	NeonBlue  = "\033[38;5;45m"
+	NeonPink  = "\033[38;5;201m"
+	White     = "\033[97m"
 )
