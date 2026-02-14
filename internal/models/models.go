c28297e994a7a9f59158fff48a6274e488656b14
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 12:40:54 -0500

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,22 +1,18 @@
 package models
 
 type IntelData struct {
-	IPs         []string
-	Nameservers []string
-	WhoisRaw    string
+	IP          string
 	Registrar   string
+	ISP         string
+	Org         string
 	Country     string
+	CountryCode string
+	RegionName  string // State
+	Zip         string
 	City        string
 	Lat         float64
 	Lon         float64
-	ISP         string
-	Org         string
-	ASN         string
-	IsAlive     bool
-}
-
-type ProbeResult struct {
-	Port     int
-	State    string
-	Service  string
+	NameServers []string
+	LocalHost   string
+	LocalIPs    []string
 }
