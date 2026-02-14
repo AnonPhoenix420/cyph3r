d85579972bb0ae7b07fe1339e7742d0bfd6db039
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 08:35:59 -0500

    Update render.go

--- internal/output/render.go
+++ internal/output/render.go
@@ -6,12 +6,8 @@ import (
 )
 
 func DisplayHUD(data models.IntelData) {
-	fmt.Printf("\n%s--- [ NODE_INTELLIGENCE_HUD ] ---%s\n", NeonPink, Reset)
+	fmt.Printf("\n%s--- [ CYPH3R_INTEL_HUD ] ---%s\n", NeonPink, Reset)
 	fmt.Printf("%s[*] Registrar: %s%s\n", White, NeonGreen, data.Registrar, Reset)
 	fmt.Printf("%s[*] Location:  %s%s, %s%s\n", White, NeonGreen, data.City, data.Country, Reset)
 	fmt.Printf("%s[*] Provider:  %s%s (%s)%s\n", White, NeonGreen, data.ISP, data.ASN, Reset)
-}
-
-func PrintScanHeader() {
-	fmt.Printf("\n%s[#] STARTING TACTICAL PORT PROBE...%s\n", NeonBlue, Reset)
 }
