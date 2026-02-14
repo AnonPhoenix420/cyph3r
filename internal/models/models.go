367e70d0ce03fe5bc7d9dfce50e4c13556ac4548
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 13:08:37 -0500

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,17 +2,16 @@ package models
 
 type IntelData struct {
 	IP          string
-	Registrar   string
-	ISP         string
+	TargetName  string 
+	TargetIPs   []string  // All resolved IPs for the target
 	Org         string
 	Country     string
 	CountryCode string
-	RegionName  string // State
+	RegionName  string 
 	Zip         string
 	City        string
 	Lat         float64
 	Lon         float64
-	NameServers []string
-	LocalHost   string
-	LocalIPs    []string
+	NameServers map[string]string // Hostname -> IP
+	PhoneNumbers string // All resolved phone numbers for the target
 }
