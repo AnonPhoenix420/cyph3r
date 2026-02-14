4b825dec678cfa926e46843b7e422f2e4e0ce71a
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 11:46:34 -0500

    Update colors.go

--- internal/output/colors.go
+++ internal/output/colors.go
@@ -1,10 +1,10 @@
 package output
 
 const (
-	Reset     = "\033[0m"
-	NeonGreen = "\033[38;5;121m"
 	NeonBlue  = "\033[38;5;45m"
 	NeonPink  = "\033[38;5;201m"
-	NeonYellow = "\033[38;5;226m"
-	White     = "\033[97m"
+	NeonGreen = "\033[38;5;82m"
+	White     = "\033[38;5;255m"
+	Red       = "\033[38;5;196m"
+	Reset     = "\033[0m"
 )
