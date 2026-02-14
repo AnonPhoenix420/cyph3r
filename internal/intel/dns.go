9aa5f39d4cd5764bd7ee5ba750d284f33de9b61b
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 10:26:07 -0500

    Update dns.go

--- internal/intel/dns.go
+++ internal/intel/dns.go
@@ -7,6 +7,8 @@ func LookupNodes(target string) ([]string, []string) {
 	addr, _ := net.LookupHost(target)
 	ips = addr
 	nsRecords, _ := net.LookupNS(target)
-	for _, ns := range nsRecords { nss = append(nss, ns.Host) }
+	for _, ns := range nsRecords {
+		nss = append(nss, ns.Host)
+	}
 	return ips, nss
 }
