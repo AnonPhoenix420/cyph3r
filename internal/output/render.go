b46a64320bcb225931cfe748dcf541c00e20d890
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 10:57:35 -0500

    Update render.go

--- internal/output/render.go
+++ internal/output/render.go
@@ -1,10 +1,17 @@
+package output
+
+import (
+	"fmt"
+	"github.com/AnonPhoenix420/cyph3r/internal/models"
+)
+
 func DisplayHUD(data models.IntelData) {
 	fmt.Printf("\n%s--- [ NODE_INTELLIGENCE_HUD ] ---%s\n", NeonPink, Reset)
 	fmt.Printf("%s[*] Registrar: %s%s\n", White, NeonGreen, data.Registrar)
 	fmt.Printf("%s[*] Provider:  %s%s\n", White, NeonGreen, data.ISP)
 	fmt.Printf("%s[*] Location:  %s%s, %s\n", White, NeonGreen, data.City, data.Country)
 	
-	// Clickable Map Link
+	// Clickable Map Link - Uses %f for float coordinates
 	mapURL := fmt.Sprintf("https://www.google.com/maps?q=%f,%f", data.Lat, data.Lon)
 	fmt.Printf("%s[*] Geo-Map:   %s%s%s\n", White, NeonBlue, mapURL, Reset)
 }
