5f99af0d6e635c66f569156961704bc44eec815c
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 10:43:11 -0500

    Update render.go

--- internal/output/render.go
+++ internal/output/render.go
@@ -1,18 +1,9 @@
-package output
-
-import (
-	"fmt"
-	"github.com/AnonPhoenix420/cyph3r/internal/models"
-)
-
-func PrintScanHeader() {
-	fmt.Printf("\n%s[#] STARTING TACTICAL PORT PROBE...%s\n", NeonBlue, Reset)
-}
-
 func DisplayHUD(data models.IntelData) {
 	fmt.Printf("\n%s--- [ NODE_INTELLIGENCE_HUD ] ---%s\n", NeonPink, Reset)
-	fmt.Printf("%s[*] Registrar: %s%s\n", White, NeonGreen, data.Registrar, Reset)
-	fmt.Printf("%s[*] Location:  %s%s, %s%s\n", White, NeonGreen, data.City, data.Country, Reset)
-	fmt.Printf("%s[*] Provider:  %s%s (%s)%s\n", White, NeonGreen, data.ISP, data.ASN, Reset)
-	fmt.Printf("%s[*] Geo:       %s%f, %f%s\n", White, NeonBlue, data.Lat, data.Lon, Reset)
+	fmt.Printf("%s[*] Registrar: %s%s\n", White, NeonGreen, data.Registrar)
+	fmt.Printf("%s[*] Location:  %s%s, %s\n", White, NeonGreen, data.City, data.Country)
+	
+	// Create a clickable Google Maps link for the terminal
+	mapURL := fmt.Sprintf("https://www.google.com/maps?q=%f,%f", data.Lat, data.Lon)
+	fmt.Printf("%s[*] Map Link:  %s%s%s\n", White, NeonBlue, mapURL, Reset)
 }
