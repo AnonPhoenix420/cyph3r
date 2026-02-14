cb24ca1e48e4042cc480750c0102cbc38c11857a
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 10:31:53 -0500

    Update status.go

--- internal/output/status.go
+++ internal/output/status.go
@@ -6,6 +6,6 @@ func Error(msg string) {
 	fmt.Printf("%s[!] ERROR: %s%s%s\n", NeonPink, White, msg, Reset)
 }
 
-func PrintStatus(label, msg string) {
-	fmt.Printf("%s[*] %s: %s%s%s\n", White, label, NeonBlue, msg, Reset)
+func PulseNode(target string) {
+	fmt.Printf("%s[*] ESTABLISHING PULSE WITH: %s%s%s\n", White, NeonPink, target, Reset)
 }
