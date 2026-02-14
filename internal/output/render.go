cbb703f7b361ff1ba8509c2f09ba0e245249f4de
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 10:46:46 -0500

    Update render.go

--- internal/output/render.go
+++ internal/output/render.go
@@ -1,9 +1,10 @@
 func DisplayHUD(data models.IntelData) {
 	fmt.Printf("\n%s--- [ NODE_INTELLIGENCE_HUD ] ---%s\n", NeonPink, Reset)
 	fmt.Printf("%s[*] Registrar: %s%s\n", White, NeonGreen, data.Registrar)
+	fmt.Printf("%s[*] Provider:  %s%s\n", White, NeonGreen, data.ISP)
 	fmt.Printf("%s[*] Location:  %s%s, %s\n", White, NeonGreen, data.City, data.Country)
 	
-	// Create a clickable Google Maps link for the terminal
+	// Clickable Map Link
 	mapURL := fmt.Sprintf("https://www.google.com/maps?q=%f,%f", data.Lat, data.Lon)
-	fmt.Printf("%s[*] Map Link:  %s%s%s\n", White, NeonBlue, mapURL, Reset)
+	fmt.Printf("%s[*] Geo-Map:   %s%s%s\n", White, NeonBlue, mapURL, Reset)
 }
