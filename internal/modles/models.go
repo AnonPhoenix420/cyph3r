caec21dfcc00c1ce277483b193e01e35c74f2af1
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 09:34:04 -0500

    Update models.go

--- internal/modles/models.go
+++ internal/modles/models.go
@@ -1,15 +1,19 @@
 package models
-
-type IntelData struct {
-	IPs         []string
-	Nameservers []string
-	WhoisRaw    string
-	Registrar   string
-	Country     string
-	City        string
-	Lat         float64
-	Lon         float64
-	ISP         string
-	Org         string
-	ASN         string
-}
+type IntelData  struct {
+	Target
+	IPs          string
+	NameServers  string
+	WhoisRaw     string
+	Registrar    string
+	Country      string
+	City         string
+	Lat          float64
+	Lon          float64
+	ISP          string
+	Org          string
+	ASN          string
+	PhoneNumbers string
+	OpenPorts    string
+	HTTPStatus   string
+	ScanResult   string
+{
