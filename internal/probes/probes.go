dc1c4d2d690f9880935591b402fbc0ba2b2d9c3c
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-14 10:26:43 -0500

    Update probes.go

--- internal/probes/probes.go
+++ internal/probes/probes.go
@@ -2,12 +2,12 @@ package probes
 
 import (
 	"fmt"
-	"cyph3r/internal/output"
+	"github.com/AnonPhoenix420/cyph3r/internal/output"
 )
 
 func RunFullScan(target string) {
 	ports := []int{21, 22, 25, 53, 80, 443, 3306, 8080}
-	output.PrintStatus("INIT_SCAN", "Probing infrastructure...")
+	output.PrintScanHeader()
 	for _, port := range ports {
 		alive, status := DialTarget(target, port)
 		if alive {
