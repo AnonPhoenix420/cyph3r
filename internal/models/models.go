2e8e29743f54f32337ff6f857149cc811107bf95
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 11:57:11 -0500

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,25 +1,28 @@
 package models
 
-type IntelData struct { //show all information in hud
+type IntelData struct { //show all information for target host in hud
 	TargetName  string
-	TargetIPs   []string
+	TargetIPs   []string // show All for target host
 	ISP         string
 	Org         string
 	Country     string
 	RegionName  string
 	State       string
 	City        string
-	Zip         string 
-	Lat         float64
-	Lon         float64
+	Zip         string // show zip code or regional area or contry code both if available 
+	Lat         string
+	Lon         string
 	NameServers map[string][]string
 }
 
 type PhoneData struct {
 	Number   string
 	Country  string
+	State    string
 	Location string
-	Carrier  string
+	lon      string
+	lat      string
+	Carrier  string // show exact service provider
 	Type     string
 	Valid    bool
 	MapLink  string // New Field for Pinpoint Mapping
