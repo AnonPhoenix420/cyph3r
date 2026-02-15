184560ac37a8443bfcb9aba7ae790e84ce81519c
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 13:15:53 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -6,56 +6,44 @@ import (
 	"net"
 	"net/http"
 	"time"
-	"github.com/AnonPhoenix420/cyph3r/internal/models"
+	"strings"
+	"cyph3r/internal/models"
 )
 
 func GetTargetIntel(input string) (models.IntelData, error) {
 	var data models.IntelData
 	data.TargetName = input
 	data.NameServers = make(map[string][]string)
-
 	ips, _ := net.LookupIP(input)
-	for _, ip := range ips {
-		data.TargetIPs = append(data.TargetIPs, ip.String())
-	}
+	for _, ip := range ips { data.TargetIPs = append(data.TargetIPs, ip.String()) }
 	
-	nsRecords, _ := net.LookupNS(input)
-	for _, ns := range nsRecords {
-		nsIPs, _ := net.LookupIP(ns.Host)
-		var addrList []string
-		for _, ip := range nsIPs {
-			addrList = append(addrList, ip.String())
-		}
-		data.NameServers[ns.Host] = addrList
-	}
-
-	if len(data.TargetIPs) > 0 {
-		client := &http.Client{Timeout: 5 * time.Second}
-		resp, err := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=status,country,regionName,city,zip,lat,lon,isp,org")
-		if err == nil {
-			defer resp.Body.Close()
-			var temp struct {
-				Lat    float64 `json:"lat"`
-				Lon    float64 `json:"lon"`
-				Region string  `json:"regionName"`
-				ISP    string  `json:"isp"`
-				Org    string  `json:"org"`
-				City   string  `json:"city"`
-				Zip    string  `json:"zip"`
-				Ctry   string  `json:"country"`
-			}
-			json.NewDecoder(resp.Body).Decode(&temp)
-			
-			data.Lat = fmt.Sprintf("%.6f", temp.Lat)
-			data.Lon = fmt.Sprintf("%.6f", temp.Lon)
-			data.State = temp.Region
-			data.ISP = temp.ISP
-			data.Org = temp.Org
-			data.City = temp.City
-			data.Zip = temp.Zip
-			data.Country = temp.Ctry
-			data.MapLink = fmt.Sprintf("https://www.google.com/maps?q=%s,%s", data.Lat, data.Lon)
-		}
+	client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=status,country,regionName,city,zip,lat,lon,isp,org")
+	if err == nil {
+		defer resp.Body.Close()
+		var t struct { Lat, Lon float64; Reg, ISP, Org, City, Zip, Ctry string }
+		json.NewDecoder(resp.Body).Decode(&t)
+		data.Lat, data.Lon = fmt.Sprintf("%.6f", t.Lat), fmt.Sprintf("%.6f", t.Lon)
+		data.State, data.ISP, data.Org, data.City, data.Zip, data.Country = t.Reg, t.ISP, t.Org, t.City, t.Zip, t.Ctry
+		data.MapLink = fmt.Sprintf("https://www.google.com/maps?q=%s,%s", data.Lat, data.Lon)
 	}
 	return data, nil
+}
+
+func GetPhoneIntel(number string) (models.PhoneData, error) {
+	var d models.PhoneData
+	d.Number, d.Valid = number, true
+	clean := strings.TrimPrefix(number, "+")
+	globalMap := map[string][]string{
+		"1330": {"USA", "Ohio", "Akron/Canton", "41.0814", "-81.5190", "Verizon/AT&T"},
+		"44":   {"UK", "United Kingdom", "London Hub", "51.5074", "-0.1278", "BT/Vodafone"},
+	}
+	for pfx, info := range globalMap {
+		if strings.HasPrefix(clean, pfx) {
+			d.Country, d.State, d.Location, d.Lat, d.Lon, d.Carrier = info[0], info[1], info[2], info[3], info[4], info[5]
+			d.MapLink = fmt.Sprintf("https://www.google.com/maps?q=%s,%s", d.Lat, d.Lon)
+			break
+		}
+	}
+	return d, nil
 }
