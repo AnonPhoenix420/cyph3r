441bbbfd177fcddb6e352f1d5955777165f32072
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 13:46:19 -0500

    Update render.go

--- internal/output/render.go
+++ internal/output/render.go
@@ -11,6 +11,12 @@ func PulseNode(target string) {
 
 func DisplayHUD(data models.IntelData) {
 	fmt.Printf("\n%s--- [ TARGET_HUD ] ---\n%s[*] Node: %s%s\n", NeonPink, White, NeonBlue, data.TargetName)
+	fmt.Printf("%s[*] Loc:  %s%s, %s\n", White, NeonGreen, data.City, data.Country)
+	fmt.Printf("%s[*] Map:  %s%s\n", White, NeonBlue, data.MapLink)
 }
 
-// Ensure internal/output/banner.go has: func DisplayBanner()
+func DisplayPhoneHUD(p models.PhoneData) {
+	fmt.Printf("\n%s--- [ SATELLITE_HUD ] ---\n%s[*] Target: %s%s\n", NeonPink, White, NeonBlue, p.Number)
+	fmt.Printf("%s[*] Carrier: %s%s\n", White, NeonGreen, p.Carrier)
+	fmt.Printf("%s[*] Vector:  %s%s\n", White, NeonBlue, p.MapLink)
+}
