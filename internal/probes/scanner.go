e7ad1b6869a6ec44d64ff81a75b35e04f4ddfd5b
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 14:23:47 -0500

    Update scanner.go

--- internal/probes/scanner.go
+++ internal/probes/scanner.go
@@ -1,23 +1,40 @@
-package intel
+package probes
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"time"
 )
 
-// ScanPorts checks common tactical ports
-func ScanPorts(target string) []int {
-	openPorts := []int{}
+// ScanPorts probes the target and grabs service banners
+func ScanPorts(target string) []string {
+	var results []string
 	commonPorts := []int{21, 22, 25, 53, 80, 110, 443, 3306, 3389, 8080}
 
 	for _, port := range commonPorts {
 		address := fmt.Sprintf("%s:%d", target, port)
-		conn, err := net.DialTimeout("tcp", address, 2*time.Second)
+		conn, err := net.DialTimeout("tcp", address, 1*time.Second)
 		if err == nil {
-			openPorts = append(openPorts, port)
+			banner := grabBanner(conn)
+			if banner != "" {
+				results = append(results, fmt.Sprintf("%d (%s)", port, banner))
+			} else {
+				results = append(results, fmt.Sprintf("%d", port))
+			}
 			conn.Close()
 		}
 	}
-	return openPorts
+	return results
+}
+
+func grabBanner(conn net.Conn) string {
+	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+	// Some services talk first (SSH, FTP), others wait for a request
+	fmt.Fprintf(conn, "HEAD / HTTP/1.0\r\n\r\n") 
+	scanner := bufio.NewScanner(conn)
+	if scanner.Scan() {
+		return scanner.Text()
+	}
+	return ""
 }
