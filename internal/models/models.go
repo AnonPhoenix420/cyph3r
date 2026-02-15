14d4625652bdee02bddebe6f37824ce462f38896
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 08:58:26 -0500

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,17 +1,27 @@
 package models
 
+// IntelData handles remote target network recon
 type IntelData struct {
 	IP          string
-	TargetName  string 
-	TargetIPs   []string  // All resolved IPs for the target
+	TargetName  string
+	TargetIPs   []string // All resolved IPs for the target
 	Org         string
 	Country     string
 	CountryCode string
-	RegionName  string 
+	RegionName  string
 	Zip         string
 	City        string
 	Lat         float64
 	Lon         float64
-	NameServers map[string]string // Hostname -> IP
-	PhoneNumbers string // All resolved phone numbers for the target
+	NameServers map[string]string // All resolved Hostname -> IP
+}
+
+// PhoneData handles international phone metadata
+type PhoneData struct {
+	Number      string `json:"number"`
+	Valid       bool   `json:"valid"`
+	LocalFormat string `json:"local_format"`
+	Carrier     string `json:"carrier"`
+	Location    string `json:"location"`
+	Type        string `json:"line_type"`
 }
