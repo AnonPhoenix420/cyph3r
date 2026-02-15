8c2529a03990680b53b1b73a929f979c6b3102cb
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 14:11:18 -0500

    Update render.go

--- internal/output/render.go
+++ internal/output/render.go
@@ -5,28 +5,36 @@ import (
 	"github.com/AnonPhoenix420/cyph3r/internal/models"
 )
 
+func PulseNode(target string) {
+	fmt.Printf("\n%s[!] Identifying Node: %s%s%s\n", White, NeonPink, target, Reset)
+}
+
 func DisplayHUD(data models.IntelData) {
 	fmt.Printf("\n%s--- [ REMOTE_TARGET_INTELLIGENCE_HUD ] ---\n", NeonPink)
 	fmt.Printf("%s[*] Node:      %s%s\n", White, NeonBlue, data.TargetName)
 	
-	// IP Stack
 	for i, ip := range data.TargetIPs {
 		fmt.Printf("%s[*] IP [%d]:    %s%s\n", White, i, NeonGreen, ip)
 	}
 
-	// Location Stack
-	fmt.Printf("%s[*] Provider:   %s%s\n", White, NeonYellow, data.ISP)
-	fmt.Printf("%s[*] Org:        %s%s\n", White, NeonYellow, data.Org)
+	fmt.Printf("%s[*] Provider:   %s%s (%s)\n", White, NeonYellow, data.ISP, data.Org)
 	fmt.Printf("%s[*] Location:   %s%s, %s, %s\n", White, NeonGreen, data.City, data.State, data.Country)
 	
-	// DNS Stack
 	if len(data.NameServers["NS"]) > 0 {
 		fmt.Printf("%s[*] NS Nodes:   %s%v\n", White, NeonBlue, data.NameServers["NS"])
 	}
 	if len(data.NameServers["MX"]) > 0 {
-		fmt.Printf("%s[*] Mail Nodes: %s%v\n", White, NeonBlue, data.NameServers["MX"])
+		fmt.Printf("%s[*] MX Nodes:   %s%v\n", White, NeonBlue, data.NameServers["MX"])
 	}
 
 	fmt.Printf("%s[*] Map Link:   %s%s\n", White, NeonBlue, data.MapLink)
 	fmt.Printf("%s------------------------------------------%s\n", NeonPink, Reset)
+}
+
+func DisplayPhoneHUD(p models.PhoneData) {
+	fmt.Printf("\n%s--- [ 🛰️ GLOBAL_SATELLITE_HUD ] ---\n", NeonPink)
+	fmt.Printf("%s[*] Target:     %s%s\n", White, NeonBlue, p.Number)
+	fmt.Printf("%s[*] Carrier:    %s%s\n", White, NeonYellow, p.Carrier)
+	fmt.Printf("%s[*] Vector:     %s%s\n", White, NeonBlue, p.MapLink)
+	fmt.Printf("%s------------------------------------%s\n", NeonPink, Reset)
 }
