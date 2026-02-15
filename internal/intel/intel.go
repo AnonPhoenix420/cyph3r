fd8241f614696b5822edf14af5a1405d8ead420d
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 09:29:02 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -1,10 +1,10 @@
 package intel
 
 import (
+	"encoding/json"
 	"net"
 	"net/http"
 	"time"
-	"encoding/json"
 	"github.com/AnonPhoenix420/cyph3r/internal/models"
 )
 
@@ -12,31 +12,28 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 	var data models.IntelData
 	data.NameServers = make(map[string]string)
 
-	parsedIP := net.ParseIP(input)
-	if parsedIP != nil {
-		data.IP = input
-		data.TargetIPs = append(data.TargetIPs, input)
-		names, _ := net.LookupAddr(input)
-		if len(names) > 0 { data.TargetName = names[0] }
-	} else {
-		data.TargetName = input
-		ips, _ := net.LookupIP(input)
-		for _, ip := range ips {
-			data.TargetIPs = append(data.TargetIPs, ip.String())
-		}
-		if len(data.TargetIPs) > 0 { data.IP = data.TargetIPs[0] }
+	// 1. Resolve ALL Target IPs (IPv4 + IPv6)
+	ips, _ := net.LookupIP(input)
+	for _, ip := range ips {
+		data.TargetIPs = append(data.TargetIPs, ip.String())
+	}
+	if len(data.TargetIPs) > 0 {
+		data.IP = data.TargetIPs[0]
 	}
 
-	nsRecords, _ := net.LookupNS(data.TargetName)
+	// 2. Resolve Name Servers with IPv4 priority for "Local" style display
+	nsRecords, _ := net.LookupNS(input)
 	for _, ns := range nsRecords {
 		nsIPs, _ := net.LookupIP(ns.Host)
 		if len(nsIPs) > 0 {
+			// Save the first resolved IP (usually IPv4)
 			data.NameServers[ns.Host] = nsIPs[0].String()
 		}
 	}
 
+	// 3. Deep Geo-Intelligence (Restoring the lost data)
 	client := &http.Client{Timeout: 5 * time.Second}
-	resp, err := client.Get("http://ip-api.com/json/" + data.IP + "?fields=status,country,countryCode,regionName,city,zip,lat,lon,isp,org")
+	resp, err := client.Get("http://ip-api.com/json/" + data.IP + "?fields=status,message,country,countryCode,regionName,city,zip,lat,lon,isp,org,as")
 	if err == nil {
 		defer resp.Body.Close()
 		json.NewDecoder(resp.Body).Decode(&data)
