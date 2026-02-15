4501790f0da76216376ba2eea5b953a6856f93a6
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 10:39:07 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -10,30 +10,29 @@ import (
 
 func GetTargetIntel(input string) (models.IntelData, error) {
 	var data models.IntelData
-	data.NameServers = make(map[string]string)
+	data.TargetName = input // FIX: Ensures the input is explicitly stored
+	data.NameServers = make(map[string][]string)
 
-	// 1. Resolve ALL Target IPs (IPv4 + IPv6)
+	// 1. Resolve ALL IPs (Dual Stack)
 	ips, _ := net.LookupIP(input)
 	for _, ip := range ips {
 		data.TargetIPs = append(data.TargetIPs, ip.String())
 	}
-	if len(data.TargetIPs) > 0 {
-		data.IP = data.TargetIPs[0]
-	}
-
-	// 2. Resolve Name Servers with IPv4 priority for "Local" style display
+	
+	// 2. Resolve Name Servers with BOTH IP types
 	nsRecords, _ := net.LookupNS(input)
 	for _, ns := range nsRecords {
+		var nsAddrList []string
 		nsIPs, _ := net.LookupIP(ns.Host)
-		if len(nsIPs) > 0 {
-			// Save the first resolved IP (usually IPv4)
-			data.NameServers[ns.Host] = nsIPs[0].String()
+		for _, ip := range nsIPs {
+			nsAddrList = append(nsAddrList, ip.String())
 		}
+		data.NameServers[ns.Host] = nsAddrList
 	}
 
-	// 3. Deep Geo-Intelligence (Restoring the lost data)
+	// 3. Geographic Deep-Dive
 	client := &http.Client{Timeout: 5 * time.Second}
-	resp, err := client.Get("http://ip-api.com/json/" + data.IP + "?fields=status,message,country,countryCode,regionName,city,zip,lat,lon,isp,org,as")
+	resp, err := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=status,country,regionName,city,zip,lat,lon,isp,org")
 	if err == nil {
 		defer resp.Body.Close()
 		json.NewDecoder(resp.Body).Decode(&data)
