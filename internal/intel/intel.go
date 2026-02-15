bbaf4f6c25e712fbdfd3ac2493db2910a4858c56
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 14:07:20 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -14,38 +14,47 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 	data.TargetName = input
 	data.NameServers = make(map[string][]string)
 
-	// 1. RESOLVE IPs
+	// 1. IP RESOLUTION (A & AAAA Records)
 	ips, _ := net.LookupIP(input)
 	for _, ip := range ips {
 		data.TargetIPs = append(data.TargetIPs, ip.String())
 	}
 
-	// 2. RESOLVE NAMESERVERS
+	// 2. DNS INFRASTRUCTURE (NS & MX & TXT)
 	ns, _ := net.LookupNS(input)
 	for _, nameserver := range ns {
-		data.NameServers["DNS"] = append(data.NameServers["DNS"], nameserver.Host)
+		data.NameServers["NS"] = append(data.NameServers["NS"], nameserver.Host)
 	}
 
-	// 3. GEOLOCATION
+	mx, _ := net.LookupMX(input)
+	for _, mail := range mx {
+		data.NameServers["MX"] = append(data.NameServers["MX"], mail.Host)
+	}
+
+	txt, _ := net.LookupTXT(input)
+	for _, t := range txt {
+		data.NameServers["TXT"] = append(data.NameServers["TXT"], t)
+	}
+
+	// 3. DEEP GEOLOCATION & ASN
 	if len(data.TargetIPs) > 0 {
 		client := &http.Client{Timeout: 5 * time.Second}
-		resp, err := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=status,country,regionName,city,zip,lat,lon,isp,org,as")
+		// Querying IP-API for the deep stack (ASN, ISP, Proxy status)
+		resp, err := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=status,country,regionName,city,zip,lat,lon,isp,org,as,mobile,proxy,hosting")
 		if err == nil {
 			defer resp.Body.Close()
 			var t struct {
 				Lat, Lon float64
 				Status, Country, RegionName, City, Zip, Isp, Org, As string
+				Mobile, Proxy, Hosting bool
 			}
 			json.NewDecoder(resp.Body).Decode(&t)
 
 			data.Lat, data.Lon = fmt.Sprintf("%.6f", t.Lat), fmt.Sprintf("%.6f", t.Lon)
-			data.Country = t.Country
-			data.State = t.RegionName
-			data.City = t.City
-			data.Zip = t.Zip
-			data.ISP = t.Isp
-			data.Org = t.Org
-			// REAL Google Maps link
+			data.Country, data.State, data.City, data.Zip = t.Country, t.RegionName, t.City, t.Zip
+			data.ISP, data.Org = t.Isp, t.Org
+			
+			// Re-adding the real Map link
 			data.MapLink = fmt.Sprintf("https://www.google.com/maps?q=%s,%s", data.Lat, data.Lon)
 		}
 	}
