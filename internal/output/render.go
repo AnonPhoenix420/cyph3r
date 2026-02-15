2c905f7d502b6f1da9a8d85d9c4e940f26ecac5c
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 14:01:40 -0500

    Update render.go

--- internal/output/render.go
+++ internal/output/render.go
@@ -5,18 +5,22 @@ import (
 	"github.com/AnonPhoenix420/cyph3r/internal/models"
 )
 
-func PulseNode(target string) {
-	fmt.Printf("\n%s[!] Identifying Node: %s%s%s\n", White, NeonPink, target, Reset)
-}
-
 func DisplayHUD(data models.IntelData) {
-	fmt.Printf("\n%s--- [ TARGET_HUD ] ---\n%s[*] Node: %s%s\n", NeonPink, White, NeonBlue, data.TargetName)
-	fmt.Printf("%s[*] Loc:  %s%s, %s\n", White, NeonGreen, data.City, data.Country)
-	fmt.Printf("%s[*] Map:  %s%s\n", White, NeonBlue, data.MapLink)
-}
+	fmt.Printf("\n%s--- [ REMOTE_TARGET_INTELLIGENCE_HUD ] ---\n", NeonPink)
+	fmt.Printf("%s[*] Node:      %s%s\n", White, NeonBlue, data.TargetName)
+	
+	if len(data.TargetIPs) > 0 {
+		fmt.Printf("%s[*] IP Vector:  %s%s\n", White, NeonGreen, data.TargetIPs[0])
+	}
 
-func DisplayPhoneHUD(p models.PhoneData) {
-	fmt.Printf("\n%s--- [ SATELLITE_HUD ] ---\n%s[*] Target: %s%s\n", NeonPink, White, NeonBlue, p.Number)
-	fmt.Printf("%s[*] Carrier: %s%s\n", White, NeonGreen, p.Carrier)
-	fmt.Printf("%s[*] Vector:  %s%s\n", White, NeonBlue, p.MapLink)
+	fmt.Printf("%s[*] Provider:   %s%s (%s)\n", White, NeonYellow, data.ISP, data.Org)
+	fmt.Printf("%s[*] Location:   %s%s, %s, %s\n", White, NeonGreen, data.City, data.State, data.Country)
+	fmt.Printf("%s[*] Lat/Lon:    %s%s, %s\n", White, NeonPink, data.Lat, data.Lon)
+	
+	if len(data.NameServers["DNS"]) > 0 {
+		fmt.Printf("%s[*] DNS Nodes:  %s%s\n", White, NeonBlue, data.NameServers["DNS"][0])
+	}
+
+	fmt.Printf("%s[*] Map Link:   %s%s\n", White, NeonBlue, data.MapLink)
+	fmt.Printf("%s------------------------------------------%s\n", NeonPink, Reset)
 }
