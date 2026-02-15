d35ad9ef6a13f280e26fc539a41d775f795f8b41
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 10:47:49 -0500

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,29 +1,27 @@
 package models
 
-// IntelData handles remote target network recon
 type IntelData struct {
-	IP          string
 	TargetName  string
 	TargetIPs   []string
-	ISP         string   // This was the missing field
+	ISP         string
 	Org         string
 	Country     string
-	CountryCode string
 	RegionName  string
-	Zip         string
+	State       string
 	City        string
+	Zip         string
 	Lat         float64
 	Lon         float64
-	NameServers map[string]string
+	Number     []string
+	NameServers map[string][]string // Changed to []string
 }
 
-// PhoneData handles international phone metadata
 type PhoneData struct {
-	Number      string `json:"number"`
-	Valid       bool   `json:"valid"`
-	LocalFormat string `json:"local_format"`
-	Carrier     string `json:"carrier"`
-	Location    string `json:"location"`
-	Type        string `json:"line_type"`
+	Number   string
+	Country  string
+	Location string
+	Carrier  string
+	Type     string
+	Valid    bool
 }
 
