399ad977d3f5626f2e4e84b1a1a9a59438b8aa97
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-15 21:20:27 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -1,10 +1,15 @@
 package intel
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net"
 	"net/http"
+	"strings"
 	"time"
+
 	"github.com/AnonPhoenix420/cyph3r/internal/models"
 	"github.com/AnonPhoenix420/cyph3r/internal/probes"
 )
@@ -14,38 +19,76 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 	data.TargetName = input
 	data.NameServers = make(map[string][]string)
 
-	// Resolve Target IPs
+	// 1. IP Resolution
 	ips, _ := net.LookupIP(input)
 	for _, ip := range ips {
 		data.TargetIPs = append(data.TargetIPs, ip.String())
 	}
 
-	// Recursive NS Resolution
+	// 2. Recursive DNS
 	ns, _ := net.LookupNS(input)
 	for _, n := range ns {
 		data.NameServers["NS"] = append(data.NameServers["NS"], n.Host)
 		nsIPs, _ := net.LookupIP(n.Host)
 		for _, nip := range nsIPs {
-			key := "IP_" + n.Host
-			data.NameServers[key] = append(data.NameServers[key], nip.String())
+			data.NameServers["IP_"+n.Host] = append(data.NameServers["IP_"+n.Host], nip.String())
 		}
 	}
-	
+
+	// 3. WHOIS (Direct Socket - No External Libs)
+	data.Org = fetchWhois(input)
+
+	// 4. Tactical Scan
 	data.NameServers["PORTS"] = probes.ScanPorts(input)
 
-	// Geo-Intel
+	// 5. Geo-Intel
 	if len(data.TargetIPs) > 0 {
-		client := &http.Client{Timeout: 5 * time.Second}
-		resp, err := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=status,country,regionName,city,zip,isp,org")
-		if err == nil {
-			defer resp.Body.Close()
-			var t struct {
-				Country, RegionName, City, Zip, Isp, Org string
-			}
-			json.NewDecoder(resp.Body).Decode(&t)
-			data.Country, data.State, data.City, data.Zip = t.Country, t.RegionName, t.City, t.Zip
-			data.ISP, data.Org = t.Isp, t.Org
+		fetchGeoData(&data)
+	}
+
+	return data, nil
+}
+
+func fetchWhois(domain string) string {
+	// Standard WHOIS server for most domains
+	whoisServer := "whois.iana.org" 
+	
+	// Step 1: Query IANA to find the real registrar server
+	conn, err := net.DialTimeout("tcp", whoisServer+":43", 5*time.Second)
+	if err != nil { return "OFFLINE" }
+	
+	fmt.Fprintf(conn, domain+"\r\n")
+	scanner := bufio.NewScanner(conn)
+	var registrar, created string
+	
+	for scanner.Scan() {
+		line := strings.ToLower(scanner.Text())
+		if strings.Contains(line, "registrar:") {
+			registrar = strings.TrimSpace(strings.Split(line, ":")[1])
+		}
+		if strings.Contains(line, "created:") {
+			created = strings.TrimSpace(strings.Split(line, ":")[1])
 		}
 	}
-	return data, nil
+	conn.Close()
+
+	if registrar != "" {
+		return fmt.Sprintf("%s (Born: %s)", registrar, created)
+	}
+	return "DATA_MASKED"
+}
+
+func fetchGeoData(data *models.IntelData) {
+	client := &http.Client{Timeout: 3 * time.Second}
+	resp, err := client.Get("http://ip-api.com/json/" + data.TargetIPs[0])
+	if err != nil { return }
+	defer resp.Body.Close()
+
+	var t struct {
+		Country, RegionName, City, Zip, Isp, Org string
+	}
+	json.NewDecoder(resp.Body).Decode(&t)
+	data.Country, data.State, data.City, data.Zip = t.Country, t.RegionName, t.City, t.Zip
+	data.ISP = t.Isp
+	if data.Org == "" { data.Org = t.Org }
 }
