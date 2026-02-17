9afff77509c3955ed57f491754ad770c33bf4c4c
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-16 21:03:05 -0500

    Update status.go

--- internal/output/status.go
+++ internal/output/status.go
@@ -4,20 +4,20 @@ import "fmt"
 
 // Error displays a critical failure message in red
 func Error(msg string) {
-	fmt.Printf("%s[!] ERROR: %s%s\n", Red, White, msg)
+	fmt.Printf("%s[!] ERROR: %s%s%s\n", Red, White, msg, Reset)
 }
 
 // Info displays a tactical information message in blue
 func Info(msg string) {
-	fmt.Printf("%s[*] INFO: %s%s\n", NeonBlue, White, msg)
+	fmt.Printf("%s[*] INFO: %s%s%s\n", NeonBlue, White, msg, Reset)
 }
 
 // Warning displays a non-critical alert in yellow
 func Warning(msg string) {
-	fmt.Printf("%s[!] WARNING: %s%s\n", NeonYellow, White, msg)
+	fmt.Printf("%s[!] WARNING: %s%s%s\n", NeonYellow, White, msg, Reset)
 }
 
 // Success displays a positive result in green
 func Success(msg string) {
-	fmt.Printf("%s[+] SUCCESS: %s%s\n", NeonGreen, White, msg)
+	fmt.Printf("%s[+] SUCCESS: %s%s%s\n", NeonGreen, White, msg, Reset)
 }
