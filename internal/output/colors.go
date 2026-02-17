fbcc3a7e79c378994ccb0378d069846f9750adce
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-16 20:55:02 -0500

    Create colors.go

---
+++ internal/output/colors.go
@@ -0,0 +1,11 @@
+package output
+
+const (
+	NeonPink   = "\033[38;5;198m"
+	NeonBlue   = "\033[38;5;39m"
+	NeonGreen  = "\033[38;5;82m"
+	NeonYellow = "\033[38;5;226m"
+	Red        = "\033[31m" 
+	White      = "\033[97m"
+	Reset      = "\033[0m"
+)
