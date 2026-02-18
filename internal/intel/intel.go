b807c84d33ff9a6f9156fc16066adcde7afd19c5
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-18 11:07:37 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -24,14 +24,15 @@ func GetClient() *http.Client {
 }
 
 func GetTargetIntel(input string) (models.IntelData, error) {
-	// Re-verify shield immediately before scan
+	// Security Handshake
 	shield := CheckShield()
 	if !shield.IsActive {
-		fmt.Println("\n\033[31m[!] PROTON VPN DISCONNECTED. EMERGENCY HALT.\033[0m")
+		fmt.Println("\n\033[31m[!] PROTON VPN NOT DETECTED. KILLING PROCESS.\033[0m")
 		os.Exit(1)
 	}
 
 	data := models.IntelData{TargetName: input, NameServers: make(map[string][]string)}
+	
 	ips, _ := net.LookupIP(input)
 	for _, ip := range ips {
 		ipStr := ip.String()
@@ -62,4 +63,64 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 	return data, nil
 }
 
-// ... [Keep detectWAF, fetchGeo, pingTarget, performTacticalScan from previous steps] ...
+// --- CORE HELPERS ---
+
+func fetchGeo(ip string) (models.GeoResponse, string) {
+	client := GetClient()
+	resp, err := client.Get("http://ip-api.com/json/" + ip + "?fields=status,country,regionName,city,isp,org,as,lat,lon,proxy,hosting,query")
+	if err != nil { return models.GeoResponse{}, "{}" }
+	defer resp.Body.Close()
+	
+	body, _ := io.ReadAll(resp.Body)
+	var r models.GeoResponse
+	json.Unmarshal(body, &r)
+	
+	var pretty interface{}
+	json.Unmarshal(body, &pretty)
+	prettyJSON, _ := json.MarshalIndent(pretty, "", "  ")
+	
+	return r, string(prettyJSON)
+}
+
+func detectWAF(target string, data *models.IntelData) {
+	client := GetClient()
+	resp, err := client.Get("http://" + target)
+	if err != nil { return }
+	defer resp.Body.Close()
+
+	server := strings.ToLower(resp.Header.Get("Server"))
+	cfRay := resp.Header.Get("CF-RAY")
+
+	if cfRay != "" || server == "cloudflare" {
+		data.IsWAF = true
+		data.WAFType = "Cloudflare (Global CDN)"
+	} else if strings.Contains(server, "arvancloud") {
+		data.IsWAF = true
+		data.WAFType = "ArvanCloud (Regional WAF)"
+	}
+}
+
+func pingTarget(ip string) string {
+	start := time.Now()
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(ip, "443"), 2*time.Second)
+	if err != nil { return "TIMEOUT" }
+	defer conn.Close()
+	return fmt.Sprintf("%dms", time.Since(start).Milliseconds())
+}
+
+func performTacticalScan(target string) []string {
+	var res []string
+	ports := []int{80, 443, 8080}
+	for _, p := range ports {
+		conn, err := net.DialTimeout("tcp", net.JoinHostPort(target, fmt.Sprintf("%d", p)), 1500*time.Millisecond)
+		if err == nil {
+			conn.Close()
+			res = append(res, fmt.Sprintf("PORT %d: OPEN", p))
+		}
+	}
+	return res
+}
+
+func GetPhoneIntel(num string) (models.PhoneData, error) {
+	return models.PhoneData{Number: num, Country: "US", Carrier: "Global"}, nil
+}
