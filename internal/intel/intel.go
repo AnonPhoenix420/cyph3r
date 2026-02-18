e04fa6399f19304ef8eaa264821c3844b070d891
AnonPhoenix420 <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-18 10:57:18 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -8,7 +8,6 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"os/exec"
 	"strings"
 	"time"
 
@@ -24,21 +23,15 @@ func GetClient() *http.Client {
 	}
 }
 
-func isVPNActive() bool {
-	data, _ := os.ReadFile("/proc/net/dev")
-	if strings.Contains(string(data), "tun") || strings.Contains(string(data), "proton") { return true }
-	out, _ := exec.Command("sh", "-c", "ip route | grep -E 'tun|wg|proton'").Output()
-	return len(out) > 0
-}
-
 func GetTargetIntel(input string) (models.IntelData, error) {
-	if !isVPNActive() {
-		fmt.Println("\n\033[31m[!] PROTON VPN NOT DETECTED. KILLING PROCESS.\033[0m")
+	// Re-verify shield immediately before scan
+	shield := CheckShield()
+	if !shield.IsActive {
+		fmt.Println("\n\033[31m[!] PROTON VPN DISCONNECTED. EMERGENCY HALT.\033[0m")
 		os.Exit(1)
 	}
 
 	data := models.IntelData{TargetName: input, NameServers: make(map[string][]string)}
-	
 	ips, _ := net.LookupIP(input)
 	for _, ip := range ips {
 		ipStr := ip.String()
@@ -47,7 +40,7 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 		if len(names) > 0 {
 			data.ReverseDNS = append(data.ReverseDNS, strings.TrimSuffix(names[0], "."))
 		} else {
-			data.ReverseDNS = append(data.ReverseDNS, "NO_PTR_FOUND")
+			data.ReverseDNS = append(data.ReverseDNS, "NO_PTR")
 		}
 	}
 	
@@ -64,63 +57,9 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 		data.Latency = pingTarget(data.TargetIPs[0])
 	}
 
-	// WAF & Cloudflare Detection Logic
 	detectWAF(input, &data)
-
 	data.ScanResults = append(data.ScanResults, performTacticalScan(input)...)
 	return data, nil
 }
 
-func detectWAF(target string, data *models.IntelData) {
-	client := GetClient()
-	resp, err := client.Get("http://" + target)
-	if err != nil { return }
-	defer resp.Body.Close()
-
-	server := resp.Header.Get("Server")
-	cfRay := resp.Header.Get("CF-RAY")
-
-	if cfRay != "" || server == "cloudflare" {
-		data.IsWAF = true
-		data.WAFType = "Cloudflare (Global CDN)"
-	} else if strings.Contains(strings.ToLower(server), "arvancloud") {
-		data.IsWAF = true
-		data.WAFType = "ArvanCloud (Regional WAF)"
-	}
-}
-
-func fetchGeo(ip string) (models.GeoResponse, string) {
-	client := GetClient()
-	resp, err := client.Get("http://ip-api.com/json/" + ip + "?fields=status,country,regionName,city,isp,org,as,lat,lon,proxy,hosting,query")
-	if err != nil { return models.GeoResponse{}, "{}" }
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	var r models.GeoResponse
-	json.Unmarshal(body, &r)
-	return r, string(body)
-}
-
-func pingTarget(ip string) string {
-	start := time.Now()
-	conn, err := net.DialTimeout("tcp", net.JoinHostPort(ip, "443"), 2*time.Second)
-	if err != nil { return "TIMEOUT" }
-	defer conn.Close()
-	return fmt.Sprintf("%dms", time.Since(start).Milliseconds())
-}
-
-func performTacticalScan(target string) []string {
-	var res []string
-	ports := []int{80, 443, 8080}
-	for _, p := range ports {
-		conn, err := net.DialTimeout("tcp", net.JoinHostPort(target, fmt.Sprintf("%d", p)), 1*time.Second)
-		if err == nil {
-			conn.Close()
-			res = append(res, fmt.Sprintf("PORT %d: OPEN", p))
-		}
-	}
-	return res
-}
-
-func GetPhoneIntel(num string) (models.PhoneData, error) {
-    return models.PhoneData{Number: num, Country: "US", Carrier: "Global"}, nil
-}
+// ... [Keep detectWAF, fetchGeo, pingTarget, performTacticalScan from previous steps] ...
