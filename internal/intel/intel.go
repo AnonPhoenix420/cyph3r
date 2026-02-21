690670b2854189795a5c93ba1f0e69fa86ed316a
Cyph3r Phoenix <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-21 11:57:33 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -11,12 +11,9 @@ import (
 )
 
 func GetTargetIntel(input string) (models.IntelData, error) {
-	data := models.IntelData{
-		TargetName:  input,
-		NameServers: make(map[string][]string),
-	}
+	data := models.IntelData{TargetName: input, NameServers: make(map[string][]string)}
 
-	// 1. DNS Cluster Recon & IP Lookup
+	// 1. Authoritative Recon
 	ns, _ := net.LookupNS(input)
 	for _, s := range ns {
 		ips, _ := net.LookupIP(s.Host)
@@ -25,6 +22,7 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 		data.NameServers[s.Host] = ipStrings
 	}
 
+	// 2. IP & PTR Recovery
 	ips, _ := net.LookupIP(input)
 	for _, ip := range ips {
 		if ip.To4() != nil { 
@@ -34,31 +32,38 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 		}
 	}
 
-	// 2. Deep Geo & Phone Data Extraction
+	// 3. Deep Telemetry
 	if len(data.TargetIPs) > 0 {
-		client := &http.Client{Timeout: 3 * time.Second}
-		resp, _ := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=66846719")
+		client := &http.Client{Timeout: 5 * time.Second}
+		resp, _ := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=mobile,proxy,hosting,org,city,regionName,country,lat,lon,status")
 		if resp != nil {
 			body, _ := io.ReadAll(resp.Body)
 			var g models.GeoResponse
 			json.Unmarshal(body, &g)
-			
-			// RESTORING DATA TO MODEL
-			data.Org = g.Org
-			data.City, data.Region, data.Country = g.City, g.RegionName, g.Country
-			data.Lat, data.Lon = g.Lat, g.Lon
-			data.IsMobile = g.Mobile
-			data.IsProxy = g.Proxy
-			data.IsHosting = g.Hosting
+			data.Org, data.City, data.Region, data.Country = g.Org, g.City, g.RegionName, g.Country
+			data.Lat, data.Lon, data.IsMobile, data.IsProxy, data.IsHosting = g.Lat, g.Lon, g.Mobile, g.Proxy, g.Hosting
 			data.RawGeo = string(body)
-			
 			resp.Body.Close()
 		}
 		data.Latency = pingTarget(data.TargetIPs[0])
 	}
-
 	analyzeWAF(input, &data)
 	return data, nil
 }
 
-// ... pingTarget and analyzeWAF functions remain the same ...
+func pingTarget(ip string) string {
+	start := time.Now()
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(ip, "443"), 1*time.Second)
+	if err != nil { return "TIMEOUT" }
+	defer conn.Close()
+	return time.Since(start).String()
+}
+
+func analyzeWAF(target string, data *models.IntelData) {
+	client := &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}, Timeout: 4 * time.Second}
+	resp, err := client.Get("https://" + target)
+	if err == nil {
+		defer resp.Body.Close()
+		if srv := resp.Header.Get("Server"); srv != "" { data.IsWAF, data.WAFType = true, srv }
+	}
+}
