6841db83cdc2dd3156987b2d2407b82c61b982c7
Cyph3r Phoenix <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-21 12:14:37 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"strings"
 	"time"
 	"github.com/AnonPhoenix420/cyph3r/internal/models"
 )
@@ -13,40 +14,41 @@ import (
 func GetTargetIntel(input string) (models.IntelData, error) {
 	data := models.IntelData{TargetName: input, NameServers: make(map[string][]string)}
 
-	// 1. Authoritative Recon
+	// DNS Cluster Recon
 	ns, _ := net.LookupNS(input)
 	for _, s := range ns {
+		host := strings.TrimSuffix(s.Host, ".")
 		ips, _ := net.LookupIP(s.Host)
 		var ipStrings []string
 		for _, ip := range ips { ipStrings = append(ipStrings, ip.String()) }
-		data.NameServers[s.Host] = ipStrings
+		data.NameServers[host] = ipStrings
 	}
 
-	// 2. IP & PTR Recovery
+	// IP Mapping
 	ips, _ := net.LookupIP(input)
 	for _, ip := range ips {
-		if ip.To4() != nil { 
-			data.TargetIPs = append(data.TargetIPs, ip.String()) 
-			names, _ := net.LookupAddr(ip.String())
-			data.ReverseDNS = append(data.ReverseDNS, names...)
-		}
+		if ip.To4() != nil { data.TargetIPs = append(data.TargetIPs, ip.String()) }
 	}
 
-	// 3. Deep Telemetry
+	// Deep Geo Fetch
 	if len(data.TargetIPs) > 0 {
 		client := &http.Client{Timeout: 5 * time.Second}
-		resp, _ := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=mobile,proxy,hosting,org,city,regionName,country,lat,lon,status")
+		resp, _ := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=66846719")
 		if resp != nil {
 			body, _ := io.ReadAll(resp.Body)
 			var g models.GeoResponse
 			json.Unmarshal(body, &g)
-			data.Org, data.City, data.Region, data.Country = g.Org, g.City, g.RegionName, g.Country
-			data.Lat, data.Lon, data.IsMobile, data.IsProxy, data.IsHosting = g.Lat, g.Lon, g.Mobile, g.Proxy, g.Hosting
+			
+			data.Org = g.Org
+			data.City, data.Region, data.Country = g.City, g.RegionName, g.Country
+			data.Lat, data.Lon = g.Lat, g.Lon
+			data.IsMobile, data.IsProxy, data.IsHosting = g.Mobile, g.Proxy, g.Hosting
 			data.RawGeo = string(body)
 			resp.Body.Close()
 		}
 		data.Latency = pingTarget(data.TargetIPs[0])
 	}
+
 	analyzeWAF(input, &data)
 	return data, nil
 }
@@ -60,10 +62,15 @@ func pingTarget(ip string) string {
 }
 
 func analyzeWAF(target string, data *models.IntelData) {
-	client := &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}, Timeout: 4 * time.Second}
+	client := &http.Client{
+		Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+		Timeout: 4 * time.Second,
+	}
 	resp, err := client.Get("https://" + target)
 	if err == nil {
 		defer resp.Body.Close()
-		if srv := resp.Header.Get("Server"); srv != "" { data.IsWAF, data.WAFType = true, srv }
+		if srv := resp.Header.Get("Server"); srv != "" { 
+			data.IsWAF, data.WAFType = true, srv 
+		}
 	}
 }
