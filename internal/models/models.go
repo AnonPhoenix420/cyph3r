becac0e825d1a5ee058480acea7c2e073814cc46
Cyph3r Phoenix <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-21 11:56:42 -0500

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -24,7 +24,6 @@ type IntelData struct {
 	RawGeo      string
 	IsWAF       bool
 	WAFType     string
-	// Restored Phone/Network Data
 	IsMobile    bool 
 	IsProxy     bool
 	IsHosting   bool
@@ -33,18 +32,13 @@ type IntelData struct {
 type GeoResponse struct {
 	Status      string  `json:"status"`
 	Country     string  `json:"country"`
-	CountryCode string  `json:"countryCode"`
 	RegionName  string  `json:"regionName"`
 	City        string  `json:"city"`
-	Zip         string  `json:"zip"`
 	Lat         float64 `json:"lat"`
 	Lon         float64 `json:"lon"`
-	Timezone    string  `json:"timezone"`
 	Isp         string  `json:"isp"`
 	Org         string  `json:"org"`
-	As          string  `json:"as"`
 	Mobile      bool    `json:"mobile"`
 	Proxy       bool    `json:"proxy"`
 	Hosting     bool    `json:"hosting"`
-	Query       string  `json:"query"`
 }
