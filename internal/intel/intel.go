7ce4a4fc530ebf75775eb6c64ad7d3ab7d9b19c7
Cyph3r Phoenix <124005680+AnonPhoenix420@users.noreply.github.com>
2026-02-21 11:32:38 -0500

    Update intel.go

--- internal/intel/intel.go
+++ internal/intel/intel.go
@@ -3,7 +3,6 @@ package intel
 import (
 	"crypto/tls"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -18,6 +17,7 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 		NameServers: make(map[string][]string),
 	}
 
+	// 1. DNS & Cluster Recon
 	ns, _ := net.LookupNS(input)
 	for _, s := range ns {
 		ips, _ := net.LookupIP(s.Host)
@@ -26,48 +26,49 @@ func GetTargetIntel(input string) (models.IntelData, error) {
 		data.NameServers[s.Host] = ipStrings
 	}
 
+	// 2. IP Mapping & Reverse DNS (The Fix)
 	ips, _ := net.LookupIP(input)
 	for _, ip := range ips {
-		if ip.To4() != nil { data.TargetIPs = append(data.TargetIPs, ip.String()) }
-	}
-
-	if len(data.TargetIPs) > 0 {
-		geo, raw := fetchGeo(data.TargetIPs[0])
-		data.Org, data.Lat, data.Lon = geo.Org, geo.Lat, geo.Lon
-		data.RawGeo = raw
-		data.Latency = pingTarget(data.TargetIPs[0])
-	}
-
-	ports := []string{"80", "443", "8080", "2082", "2083", "2086", "2087"}
-	for _, p := range ports {
-		conn, err := net.DialTimeout("tcp", net.JoinHostPort(input, p), 400*time.Millisecond)
-		if err == nil {
-			data.ScanResults = append(data.ScanResults, "PORT "+p+": OPEN")
-			conn.Close()
+		if ip.To4() != nil { 
+			data.TargetIPs = append(data.TargetIPs, ip.String()) 
+			names, _ := net.LookupAddr(ip.String())
+			data.ReverseDNS = append(data.ReverseDNS, names...)
 		}
+	}
+
+	// 3. Deep Geo & Tactical Flags (The Fix)
+	if len(data.TargetIPs) > 0 {
+		client := &http.Client{Timeout: 3 * time.Second}
+		// Fetching all fields including proxy/hosting/mobile
+		resp, _ := client.Get("http://ip-api.com/json/" + data.TargetIPs[0] + "?fields=66846719")
+		if resp != nil {
+			body, _ := io.ReadAll(resp.Body)
+			var g models.GeoResponse
+			json.Unmarshal(body, &g)
+			data.Org = g.Org
+			data.City, data.Region, data.Country = g.City, g.RegionName, g.Country
+			data.Lat, data.Lon = g.Lat, g.Lon
+			data.RawGeo = string(body)
+			
+			// Inject flags into ScanResults for the HUD
+			if g.Proxy { data.ScanResults = append(data.ScanResults, "DETECTION: PROXY/VPN") }
+			if g.Hosting { data.ScanResults = append(data.ScanResults, "DETECTION: DATACENTER") }
+			if g.Mobile { data.ScanResults = append(data.ScanResults, "DETECTION: MOBILE_NET") }
+			resp.Body.Close()
+		}
+		data.Latency = pingTarget(data.TargetIPs[0])
 	}
 
 	analyzeWAF(input, &data)
 	return data, nil
 }
 
-func fetchGeo(ip string) (models.GeoResponse, string) {
-	client := &http.Client{Timeout: 3 * time.Second}
-	resp, err := client.Get("http://ip-api.com/json/" + ip)
-	if err != nil { return models.GeoResponse{}, "" }
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	var r models.GeoResponse
-	json.Unmarshal(body, &r)
-	return r, string(body)
-}
-
 func pingTarget(ip string) string {
 	start := time.Now()
-	conn, err := net.DialTimeout("tcp", net.JoinHostPort(ip, "443"), 1*time.Second)
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(ip, "443"), 1000*time.Millisecond)
 	if err != nil { return "TIMEOUT" }
 	defer conn.Close()
-	return fmt.Sprintf("%dms", time.Since(start).Milliseconds())
+	return time.Since(start).String()
 }
 
 func analyzeWAF(target string, data *models.IntelData) {
@@ -75,12 +76,11 @@ func analyzeWAF(target string, data *models.IntelData) {
 		Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
 		Timeout: 4 * time.Second,
 	}
-	resp, err := client.Get("http://" + target)
+	resp, err := client.Get("https://" + target)
 	if err != nil { return }
 	defer resp.Body.Close()
 	srv := resp.Header.Get("Server")
-	if strings.Contains(strings.ToLower(srv), "arvan") {
-		data.IsWAF, data.WAFType = true, "ArvanCloud (Regional WAF)"
+	if srv != "" {
+		data.IsWAF, data.WAFType = true, srv
 	}
-	data.ScanResults = append(data.ScanResults, "STACK: "+srv)
 }
