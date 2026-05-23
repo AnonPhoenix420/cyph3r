1133ff7139c32e4a4fd54c623d830c50796fc91e
Cyph3r Phoenix <124005680+AnonPhoenix420@users.noreply.github.com>
2026-05-23 17:56:35 -0400

    Refactor models and introduce new data structures

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,50 +1,27 @@
 package models
 
-type TacticalConfig struct {
-	Target string
-	Vector string
-	PPS    int
-	Port   string
-	Power  int
+import "time"
+
+type IntelPayload struct {
+	Target       string             `json:"target"`
+	ScanTime     time.Time          `json:"scan_time"`
+	ASN          string             `json:"asn,omitempty"`
+	ISP          string             `json:"isp,omitempty"`
+	Geo          GeoData            `json:"geo,omitempty"`
+	Clusters     []NamespaceCluster `json:"authoritative_clusters,omitempty"`
+	Verbose      bool               `json:"-"`
+	OutputFormat string             `json:"-"`
 }
 
-type IntelData struct {
-	TargetName  string
-	TargetIPs   []string
-	TargetIPv6s []string
-	ReverseDNS  []string
-	Org         string
-	ISP         string 
-	AS          string 
-	City        string
-	Region      string
-	RegionName  string 
-	Country     string
-	CountryCode string 
-	Zip         string 
-	Timezone    string 
-	Lat         float64
-	Lon         float64
-	NameServers map[string][]string
-	ScanResults []string
-	IsWAF       bool
-	WAFType     string
-	IsHosting   bool
+type GeoData struct {
+	Country  string `json:"country,omitempty"`
+	Region   string `json:"region,omitempty"`
+	RegionID string `json:"region_id,omitempty"`
+	City     string `json:"city,omitempty"`
+	Timezone string `json:"timezone,omitempty"`
 }
 
-type GeoResponse struct {
-	Status      string  `json:"status"`
-	Country     string  `json:"country"`
-	CountryCode string  `json:"countryCode"`
-	Region      string  `json:"region"`
-	RegionName  string  `json:"regionName"`
-	City        string  `json:"city"`
-	Zip         string  `json:"zip"`
-	Lat         float64 `json:"lat"`
-	Lon         float64 `json:"lon"`
-	Timezone    string  `json:"timezone"`
-	Isp         string  `json:"isp"`
-	Org         string  `json:"org"`
-	As          string  `json:"as"`
-	Hosting     bool    `json:"hosting"`
+type NamespaceCluster struct {
+	NameServer string   `json:"nameserver"`
+	IPs        []string `json:"ips,omitempty"`
 }
